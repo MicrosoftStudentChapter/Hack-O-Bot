9cbd9529e968929ec10568b6625b9cef52682a0f
Aryan-401 <aryan.raj.garg@gmail.com>
2022-10-09 02:28:47 +0530

    Added COG Support into Bot

--- discord_main.py
+++ discord_main.py
@@ -1,7 +1,9 @@
+import asyncio
 import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+import messages
 
 load_dotenv()
 
@@ -22,8 +24,36 @@ async def invite(ctx):
                     f"Server\n\nhttps://discord.com/api/oauth2/authorize?client_id=1028024794081394688&permissions"
                     f"=172942961728&scope=bot")
 
-@client.command()
-async def ping(ctx):
-	await ctx.channel.send(embed=discord.Embed(title="Pong!", description=f"Latency: {round(client.latency * 1000)}ms", color=discord.Color.blue()))
 
-client.run(str(os.getenv('DISCORD_TOKEN')))
+@client.command(help='Get the bot\'s latency')
+async def ping(ctx):
+    await ctx.channel.send(embed=discord.Embed(title="Pong!", description=f"Latency: {round(client.latency * 1000)}ms",
+                                               color=discord.Color.blue()))
+
+
+@client.command(help='Enables Different Categories\nAccess: Administrator')
+@commands.check_any(commands.is_owner())
+async def enable(ctx, extension: str):
+    await client.load_extension(f'cogs.{extension.lower()}')
+    success_message = await ctx.send(embed=messages.success())
+    await success_message.add_reaction('✔')
+
+
+@client.command(help='Disables Different Categories\nAccess: Administrator')
+@commands.check_any(commands.is_owner())
+async def disable(ctx, extension: str):
+    await client.unload_extension(f'cogs.{extension.lower()}')
+    success_message = await ctx.send(embed=messages.success())
+    await success_message.add_reaction('✔')
+
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            print(f"Loading {filename[:-3]}")
+            await client.load_extension(f'cogs.{filename[:-3]}')
+
+async def main():
+    await load()
+    await client.start(os.getenv('DISCORD_TOKEN'))
+
+asyncio.run(main())
