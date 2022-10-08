6077bbce945e6627f90090c58f7c622bb85e8bcf
Satyam Sagar <satyamsagar1711@gmail.com>
2022-10-09 02:31:47 +0530

    updated help embed

--- help.py
+++ help.py
@@ -8,17 +8,22 @@ class CustomHelpCommand(commands.HelpCommand):
         This is triggered when .help is invoked.
         """
         filtered = await self.filter_commands(self.context.bot.commands, sort=True)
-        embed  = discord.Embed(title="Help", description="Available commands : ", color=discord.Color.blue())
-        for command in filtered:
-            embed.add_field(name=command.name, value=command.help, inline=False)
-        embed.set_footer(text=f"Requested by {self.context.author.display_name}")
+
+        embed  = discord.Embed(title="Help Command", description="", color=discord.Color.blue())
+        embed.add_field(name="Available Commands", value="`" + "`, `".join([command.name for command in filtered]) + "`", inline=False)
+        embed.set_footer(text=f"Type '.help <CommandName>' for details on a command")
         await self.context.send(embed=embed)
 
     async def send_command_help(self, command):
         """This is triggered when .help <command> is invoked."""
-        await self.context.send(embed=discord.Embed(title=command.name, description=command.help, color=discord.Color.blue()))
+        embed = discord.Embed(title=f"Help Command: {command.name}", description="", color=discord.Color.blue())
+        embed.add_field(name=f".{command.name}", value=command.help, inline=False)
+        await self.context.send(embed=embed)
 
 
     async def send_cog_help(self, cog):
         """This is triggered when .help <cog> is invoked."""
-        await self.context.send(embed=discord.Embed(title=cog.qualified_name, description=cog.description, color=discord.Color.blue()))
+        embed = discord.Embed(title=f"Help Command: {cog.qualified_name}", description="", color=discord.Color.blue())
+        for command in cog.get_commands():
+            embed.add_field(name=f".{command.name}", value=command.help, inline=False)
+        await self.context.send(embed=embed)
