adcfefa57f8d4b82386d655cb816d669d29ff48f
Satyam Sagar <satyamsagar1711@gmail.com>
2022-10-10 23:58:55 +0530

    added horoscope command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,7 +1,36 @@
 import discord
 from discord.ext import commands
 import random
+import requests
+from datetime import datetime
 
+
+def get_zodiac(date, month):
+    if month == 12:
+        astro_sign = 'sagittarius' if (date < 22) else 'capricorn'
+    elif month == 1:
+        astro_sign = 'capricorn' if (date < 20) else 'aquarius'
+    elif month == 2:
+        astro_sign = 'aquarius' if (date < 19) else 'pisces'
+    elif month == 3:
+        astro_sign = 'pisces' if (date < 21) else 'aries'
+    elif month == 4:
+        astro_sign = 'aries' if (date < 20) else 'taurus'
+    elif month == 5:
+        astro_sign = 'taurus' if (date < 21) else 'gemini'
+    elif month == 6:
+        astro_sign = 'gemini' if (date < 21) else 'cancer'
+    elif month == 7:
+        astro_sign = 'cancer' if (date < 23) else 'leo'
+    elif month == 8:
+        astro_sign = 'leo' if (date < 23) else 'virgo'
+    elif month == 9:
+        astro_sign = 'virgo' if (date < 23) else 'libra'
+    elif month == 10:
+        astro_sign = 'libra' if (date < 23) else 'scorpio'
+    elif month == 11:
+        astro_sign = 'scorpio' if (date < 22) else 'sagittarius'
+    return astro_sign
 
 class Fun(commands.Cog):
     def __init__(self, client):
@@ -35,6 +64,33 @@ class Fun(commands.Cog):
                                            description=f'Question: {question}\nAnswer: {random.choice(responses)}',
                                            colour=discord.Colour.blurple()))
 
+
+    @commands.command(help='', name='horoscope')
+    async def horoscope(self, ctx):
+        dob = ctx.message.author.created_at
+        date = dob.day
+        month = dob.month
+        zodiac = get_zodiac(date, month)
+        # print(date, month, zodiac)
+        params = (
+            ('sign', zodiac),
+            ('day', 'today'),
+        )
+
+        content = requests.post('https://aztro.sameerkumar.website/', params=params)
+        content = content.json()
+
+        embed = discord.Embed(title=f"Horoscope: {zodiac.capitalize()}", description=f"{content['description']}", color=discord.Colour.blue())
+        embed.add_field(name="Compatibility", value=f"{content['compatibility']}", inline=True)
+        embed.add_field(name="Mood", value=f"{content['mood']}", inline=True)
+        embed.add_field(name="Color", value=f"{content['color']}", inline=False)
+        embed.add_field(name="Lucky Number", value=f"{content['lucky_number']}", inline=True)
+        embed.add_field(name="Lucky Time", value=f"{content['lucky_time']}", inline=True)
+
+        embed.set_footer(text=f"Requested by {ctx.message.author.display_name}")
+        embed.timestamp = datetime.utcnow()
+
+        await ctx.send(embed=embed)
 
 async def setup(client):
     await client.add_cog(Fun(client))
